cb37874828d1d912c0cf06c4c8837b4c587c84f6
David <DRhoton@vinyacastelldefels.com>
2021-07-15 16:56:23 +0200

    add passport-local and new localstrategy

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
+const LocalStrategy = require('passport-local');
 const ObjectID = require('mongodb').ObjectID;
 
 const app = express();
@@ -40,6 +41,17 @@ myDB(async client => {
       done(null, doc);
     });
   });
+  passport.use(new LocalStrategy(
+  function(username, password, done) {
+    myDataBase.findOne({ username: username }, function (err, user) {
+      console.log('User '+ username +' attempted to log in.');
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (password !== user.password) { return done(null, false); }
+      return done(null, user);
+    });
+  }
+));
 })
 .catch(e => {
   app.route('/').get((req, res) => {
