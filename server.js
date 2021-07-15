e10c3ccd03b7656f656847c47cfde5979e8ca5cc
David <DRhoton@vinyacastelldefels.com>
2021-07-15 16:33:45 +0200

    Try again with render route shortened to 'pug'

--- server.js
+++ server.js
@@ -26,45 +26,40 @@ app.use(passport.session());
 
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
-
-  // Be sure to change the title
   app.route('/').get((req, res) => {
-    //Change the response to render the Pug template
-    res.render(process.cwd() + '/views/pug/index', {
+    res.render('pug', {
       title: 'Connected to Database',
       message: 'Please login'
     });
   });
-
-  // Serialization and deserialization here...
   passport.serializeUser((user, done) => {
     done(null, user._id);
   });
   passport.deserializeUser((id, done) => {
-      myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
       done(null, doc);
     });
   });
-  // Be sure to add this...
-  }).catch(e => {
-    app.route('/').get((req, res) => {
-    res.render(process.cwd() + '/views/pug/index', { title: e, message: 'Unable to login' });
+})
+.catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('pug', {
+      title: e,
+      message: 'Unable to login'
+    });
   });
 });
-// app.listen out here...
 
-
-
-
-// app.route('/').get((req, res) => {
-//   // old: res.render('pug/index.pug');
-//   res.render(process.cwd() + '/views/pug/index',
-//     {
-//       title: 'Hello',
-//       message: 'Please login'
-//     }
-//   );
-// });
+// original render command no longer needed
+/* app.route('/').get((req, res) => {
+  // old: res.render('pug/index.pug');
+  res.render(process.cwd() + '/views/pug/index',
+    {
+      title: 'Hello',
+      message: 'Please login'
+    }
+  );
+}); */
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
