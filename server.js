db72ff66e628704eb1510d185b0db3e23dd7c9f5
David <DRhoton@vinyacastelldefels.com>
2021-07-17 15:06:37 +0200

    Clean Up project with routes and auth modules

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const ObjectID = require('mongodb').ObjectID;
-const bcrypt = require('bcrypt');
+const routes = require('./routes.js');
+const auth = require('./auth.js');
 
 const app = express();
 app.set('view engine', 'pug');
@@ -28,94 +27,8 @@ app.use(passport.session());
 
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
-  app.route('/').get((req, res) => {
-    res.render('pug', {
-      title: 'Connected to Database',
-      message: 'Please login',
-      showLogin: true,
-      showRegistration: true
-    });
-  });
-
-  app.post('/login',
-    passport.authenticate('local', { failureRedirect: '/' }),
-    (req, res) => {
-      res.redirect('/profile');
-    });
-
-  app.get('/profile', ensureAuthenticated, (req, res) => {
-    res.render(process.cwd() + '/views/pug/profile', {
-      username: req.user.username
-    });
-  });
-
-  app.route('/logout')   // vary from app.get('logout', (req, res) =>... for fun
-    .get((req, res) => {
-      req.logout();
-      res.redirect('/');
-  });
-
-  app.route('/register')
-    .post((req, res, next) => {
-      myDataBase.findOne({ username: req.body.username }, (err, user) => {
-        if (err) {
-          next(err);
-        } else if (user) {
-          res.redirect('/');
-        } else {
-          const hash = bcrypt.hashSync(req.body.password, 12);
-          myDataBase.insertOne({
-            username: req.body.username,
-            password: hash
-          },
-            (err, doc) => {
-              if (err) {
-                res.redirect('/');
-              } else {
-                // The inserted document is held within
-                // the ops property of the doc
-                next(null, doc.ops[0]);
-              }
-            }
-          )
-        }
-      })
-    },
-    passport.authenticate('local', { failureRedirect: '/' }),
-      (req, res, next) => {
-        res.redirect('/profile');
-      }
-  );
-
-  app.use((req, res, next) => {
-    res.status(404)
-      .type('text')
-      .send('Not Found');
-  });
-
-  passport.serializeUser((user, done) => {
-    done(null, user._id);
-  });
-  passport.deserializeUser((id, done) => {
-    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, doc);
-    });
-  });
-
-  passport.use(new LocalStrategy(
-    (username, password, done) => {
-      myDataBase.findOne({ username: username }, function (err, user) {
-        console.log('User '+ username +' attempted to log in.');
-        if (err) { return done(err); }
-        if (!user) { return done(null, false); }
-        if (!bcrypt.compareSync(password, user.password)) {
-          return done(null, false);
-        }
-        return done(null, user);
-      });
-    }
-  ));
-})
+  routes(app, myDataBase);
+  auth(app, myDataBase);
 .catch(e => {
   app.route('/').get((req, res) => {
     res.render('pug', {
@@ -124,24 +37,6 @@ myDB(async client => {
     });
   });
 });
-
-const ensureAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/');
-};
-
-// original render command no longer needed
-/* app.route('/').get((req, res) => {
-  // old: res.render('pug/index.pug');
-  res.render(process.cwd() + '/views/pug/index',
-    {
-      title: 'Hello',
-      message: 'Please login'
-    }
-  );
-}); */
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
